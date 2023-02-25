874bf7313c49b27ac45d14a303077af0f411b4aa
Ravi Mehto <118646895+R-A-V-I@users.noreply.github.com>
2023-02-25 15:50:21 +0530

    Update IsLogIn.js

--- src/Component/IsLogIn.js
+++ src/Component/IsLogIn.js
@@ -1,18 +1,10 @@
 import './login.css'
-// import { useState } from 'react';
 function IsLogIn(props){
-        // let[loginTure,updatestate]=useState(true)
-        //     function onEmailEventHandler(event){
-        //         console.log(event.target.value)
-            
-        // }
-        // inLoginhHandler(){
-        //     props.onLogin
-        // }
+        
     return(
 
         <div className='form-wrapper'>
-            {/* <form onsubmit> */}
+            
                 <div  className="log-form">
 
                     <div className='userid'>
@@ -28,12 +20,12 @@ function IsLogIn(props){
                             <input type={'password'} id="Password" placeholder=" Please enter at least 7 character"></input>
                     </div>
                     <div className='logsubmit'>
-                    <button onClick={props.OnLogin}>Log In</button>
+                        <button onClick={props.OnLogin}>Log In</button>
                     </div>
                     
 
                 </div>
-            {/* </form> */}
+           
 
             
         </div>
